9ea4322de6f107e13f3c961e424639cebd296afe
Tim Hou <thou5686@gmail.com>
2016-03-21 18:54:21 -0600

    modified controller

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask
-import numpy as np
 
 # initialization
 app = Flask(__name__)
@@ -9,9 +8,17 @@ app.config.update(
 )
 
 # controllers
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'ico/favicon.ico')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route("/")
-def hello():
-    return "Hello from Python!"
+def index():
+    return render_template('index.html')
 
 # launch
 if __name__ == "__main__":
